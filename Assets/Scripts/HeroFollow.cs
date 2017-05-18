cc271a50ba123ddbaf677a5c16f512b0296c8f95
Danyil Karuna <kd@oked.me>
2017-05-18 14:53:00 +0300

    First Homework

---
+++ Assets/Scripts/HeroFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public RabbitBehavior rabbit;
+	// Update is called once per frame
+	void Update () {
+		Transform rabbit_transform = rabbit.transform;
+		Transform camera_transform = this.transform;
+		Vector3 rabbit_position = rabbit_transform.position;
+		Vector3 camera_position = camera_transform.position;
+		camera_position.x = rabbit_position.x; camera_position.y = rabbit_position.y;
+		camera_transform.position = camera_position;
+	}
+}
---
+++ Assets/Scripts/RabbitBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitBehavior : MonoBehaviour {
+
+	public float speed = 1;
+	Rigidbody2D body = null;
+	SpriteRenderer sr = null;
+
+	// Use this for initialization
+	void Start () {
+		body = this.GetComponent<Rigidbody2D> ();
+		sr = this.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		float value = Input.GetAxis("Horizontal");
+		if (Mathf.Abs(value) > 0.0) {
+			Vector2 vel = body.velocity;
+			vel.x = value * speed;
+			body.velocity = vel;
+		}
+
+		if (value > 0) {
+			sr.flipX = false;
+		} else if (value < 0) {
+			sr.flipX = true;
+		}
+	}
+}
